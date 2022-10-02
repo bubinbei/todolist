9e6fe6f2243e6a47c3b48774c475c490f90cd0d0
Roma <bubinbei@yandex.ru>
2022-10-02 20:57:49 +0300

    remTask_v0.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import TodoList from "./TodoList";
 
 function App() {
     const todoListTitle = "What to learn"
-    const tasks = [
+    const [tasks,setTask]= useState([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "React", isDone: false},
-    ]
+        {id: 4, title: "TS", isDone: false},
+    ])
+    const remTask = (taskId: number) => setTask(tasks.filter(el=>el.id !== taskId))
 
-    const todoListTitle_2 = "What to buy"
-    const tasks_2 = [
-        {id: 1, title: "Beer", isDone: true},
-        {id: 2, title: "Cheese", isDone: false},
-        {id: 3, title: "Fish", isDone: false},
-    ]
+    // const todoListTitle_2 = "What to buy"
+    // const tasks_2 = [
+    //     {id: 1, title: "Beer", isDone: true},
+    //     {id: 2, title: "Cheese", isDone: false},
+    //     {id: 3, title: "Fish", isDone: false},
+    // ]
 
     return (
         <div className="App">
-            <TodoList tasks={tasks} title={todoListTitle}/>
-            <TodoList tasks={tasks_2} title={todoListTitle_2}/>
+            <TodoList tasks={tasks} title={todoListTitle} remTask={remTask}/>
+            {/* <TodoList tasks={tasks_2} title={todoListTitle_2} remTask={remTask}/> */}
 
         </div>
     );
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -3,13 +3,22 @@ import React from 'react';
 type TodoListPropsType ={
     title: string
     tasks: Array<TaskType>
+    remTask:(taskId: number) => void
 }
+
 type TaskType = {
     id: number
     title: string
     isDone: boolean
 }
 const TodoList = (props: TodoListPropsType) => {
+  const listing = props.tasks.map(el=>{
+  return <li>
+         <input type="checkbox" checked={el.isDone}></input>
+         <span>{el.title}</span>
+         <button onClick={()=>props.remTask(el.id)}>x</button>
+         </li>
+})
     return (
                 <div>
                     <h3>{props.title}</h3>
@@ -18,9 +27,8 @@ const TodoList = (props: TodoListPropsType) => {
                         <button>+</button>
                     </div>
                     <ul>
-                        <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-                        <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-                        <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+                      {listing}
+                      
                     </ul>
                     <div>
                         <button>All</button>
